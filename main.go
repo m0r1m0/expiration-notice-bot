f7c467da167d27a62b33435ab9c68f86e9e4c90d
pywd <yuki.wd7@gmail.com>
2019-10-23 09:12:59 +0900

    firestoreからのしゅとく処理

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
 
+	"cloud.google.com/go/firestore"
 	"github.com/line/line-bot-sdk-go/linebot"
+	"google.golang.org/api/iterator"
 )
 
 func main() {
@@ -15,6 +19,7 @@ func main() {
 	}
 
 	http.HandleFunc("/callback", func(w http.ResponseWriter, req *http.Request) {
+		getFirestore(bot)
 		events, err := bot.ParseRequest(req)
 		if err != nil {
 			if err == linebot.ErrInvalidSignature {
@@ -39,4 +44,31 @@ func main() {
 	if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func getFirestore(bot *linebot.Client) {
+	projectID := "expiration-notice-bot"
+	ctx := context.Background()
+	client, err := firestore.NewClient(ctx, projectID)
+	if err != nil {
+		log.Fatalf("Failed to create client: %v", err)
+	}
+
+	defer client.Close()
+
+	iter := client.Collection("foods").Documents(ctx)
+	// docs, _ := iter.GetAll()
+	// return docs
+	messages := []linebot.SendingMessage{}
+	for {
+		doc, err := iter.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Failed to iterate: %v", err)
+		}
+		messages = append(messages, linebot.NewTextMessage(fmt.Sprintf("%v", doc.Data())))
+	}
+	bot.PushMessage(os.Getenv("GROUP_ID"), messages...).Do()
 }
